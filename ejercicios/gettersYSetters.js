9766f177551862f5b5c31f511975c25b9daab11d
andres22 <afpinchao8@misena.edu.co>
2021-02-03 16:53:43 -0500

    Fundamentos Intermedios

---
+++ ejercicios/gettersYSetters.js
@@ -0,0 +1,6 @@
+//features nuevos que trae js 
+  //Son funciones que podemos utilizar dentro de objetos
+    //Que nos permiten obtener objetos virtual
+      //Es decir no es una propiedad que viene directamente del objeto
+        //Pero atraves de un getter y un setter podemos correr una funcion
+          //que va a calcular estos valores o va a usar un valor para establecer este nuevo valor
